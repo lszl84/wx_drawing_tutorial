8020cc3a2b900261001db11ae423235d575eb1ba
Lukasz <lukasz.sromek@gmail.com>
2021-12-14 13:56:39 +0100

    listbook to display both the rectangles and chart

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <wx/wx.h>
+#include <wx/listbook.h>
 
 #include <random>
+
 #include "drawingcanvas.h"
 #include "chartcontrol.h"
 
@@ -42,15 +44,35 @@ bool MyApp::OnInit()
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
+    auto tabs = new wxListbook(this, wxID_ANY, wxDefaultPosition, this->FromDIP(wxSize(640, 480)), wxNB_TOP);
+    tabs->SetInternalBorder(0);
+
+    wxSizer *mainSizer = new wxBoxSizer(wxVERTICAL);
+    mainSizer->Add(tabs, 1, wxEXPAND);
+
     wxSizer *sizer = new wxBoxSizer(wxVERTICAL);
 
-    chart = new ChartControl(this, wxID_ANY, wxDefaultPosition, this->FromDIP(wxSize(640, 480)));
+    wxPanel *drawingPaneWithButtons = new wxPanel(tabs);
+    auto buttonPanel = createButtonPanel(drawingPaneWithButtons);
+    canvas = new DrawingCanvas(drawingPaneWithButtons, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+    canvas->Bind(CANVAS_RECT_ADDED, &MyFrame::OnRectAdded, this);
+    canvas->Bind(CANVAS_RECT_REMOVED, &MyFrame::OnRectRemoved, this);
+
+    sizer->Add(buttonPanel, 0, wxEXPAND | wxALL, 0);
+    sizer->Add(canvas, 1, wxEXPAND | wxALL, 0);
+
+    drawingPaneWithButtons->SetSizerAndFit(sizer);
+
+    tabs->AddPage(drawingPaneWithButtons, "Rectangles");
+
+    chart = new ChartControl(tabs, wxID_ANY, wxDefaultPosition, wxDefaultSize);
     chart->title = "Important Chart";
     chart->values = {0.34, -0.17, 0.98, 0.33};
 
-    sizer->Add(chart, 1, wxEXPAND | wxALL, 0);
+    tabs->AddPage(chart, "Chart");
+    tabs->SetSelection(1);
 
-    this->SetSizerAndFit(sizer);
+    this->SetSizerAndFit(mainSizer);
 
     CreateStatusBar(1);
     SetStatusText("Ready", 0);
